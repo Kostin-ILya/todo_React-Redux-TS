736340700330ca70a401a43f44b61cdadf8c16a9
Ilya <lord-ilja@yandex.ru>
2024-09-04 22:06:54 +0300

    add Header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
+import Header from 'components/Header/Header'
+
 import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="content">
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss'
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="logo">todos</h1>
+    </header>
+  )
+}
+
+export default Header
